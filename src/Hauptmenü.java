669fce8c76b6b5435dc132dac0949042f8db380f
Maurice Haghighi Pajouh <94959873+MauriceHaghighiPajouh@users.noreply.github.com>
2022-02-22 12:10:57 +0100

    Initial commit

---
+++ src/Hauptmenü.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Hauptmenü {
+    private JButton bestellenButton1;
+    private JPanel panel1;
+    private JButton infoBestandButton;
+    private JButton verkaufenButton1;
+}
